0eb457a18572cad8e26ac9c7e1d8feed1deaeade
Akshat Sinha <akshatsinhaofficial556@gmail.com>
2022-02-21 18:24:34 +0530

    init

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,5 @@
 import React from "react";
 
-const Card = ({ name, id, email }) => {
-  return (
-    <div className="tc bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
-      <img alt="robot" src={'https://robohash.org/coolrobo'}/>
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    </div>
-  );
-};
+
 
 export default Card;
